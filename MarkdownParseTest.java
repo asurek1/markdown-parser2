28c4a6a7e4c2fe6b726fedb41b1363d0f3f2c651
asurek1 <asurek@ucsd.edu>
2022-04-23 01:35:36 -0700

    test case

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,36 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class MarkdownParseTest {
+   @Test
+   public void addition() {
+       assertEquals(2, 1 + 1);
+   }
 
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
+   @Test
+   public void testLinks() throws IOException{
+       Path fileName = Path.of("/Users/alexsurek/Documents/GitHub/markdown-parser/test-file.md");
+       String content = Files.readString(fileName) ;
+       MarkdownParse obj = new MarkdownParse();
+       ArrayList<String> links = obj.getLinks(content);
+       assertEquals(List.of("https://something.com", "some-thing.html"), links);
+   }
 
+   @Test
+   public void testFile2() throws IOException{
+       Path fileName = Path.of("/Users/alexsurek/Documents/GitHub/markdown-parser/test-file2.md");
+       String content = Files.readString(fileName);
+       MarkdownParse obj = new MarkdownParse();
+       ArrayList<String> links = obj.getLinks(content);
+       assertEquals(List.of("https://something.com", "some-thing.html"), links);
+   }
 }
+
+
+
+   
